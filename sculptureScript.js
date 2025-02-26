addfb2aac4afaa8501e9b5e4e5d476408a6261c2
Vinson Thai <121653369+vinson099@users.noreply.github.com>
2025-02-25 17:18:33 -0800

    Update sculptureScript.js

--- sculptureScript.js
+++ sculptureScript.js
@@ -2,14 +2,16 @@ const sculptureList = require('./data.js'); // import data.js
 
 sculptureListLengths = [];
 
-for(const sculpture in sculptureList){
-    const currentSculpture = sculptureList[sculpture];
-    for(const key in sculptureList[sculpture]){
-        currentSculpture[key] = currentSculpture[key].length;
+for(const sculpture in sculptureList){                          // for each sculpture in sculptureList
+    const currentSculpture = sculptureList[sculpture];          // create temporary duplicate
+    for(const key in sculptureList[sculpture]){                 // for each key in sculpture
+        currentSculpture[key] = currentSculpture[key].length;   // override value with length of value
     }
+    // add to sculptureListLengths
     sculptureListLengths.push(currentSculpture);
 }
 
+// print out the new list
 for (const sculpture in sculptureListLengths){
     console.log(sculptureListLengths[sculpture]);
 }
