04df2993d8417f20f3ae0dc1870d50fffd5d1748
Vinson Thai <121653369+vinson099@users.noreply.github.com>
2025-02-25 17:15:48 -0800

    Update sculptureScript.js

--- sculptureScript.js
+++ sculptureScript.js
@@ -3,17 +3,11 @@ const sculptureList = require('./data.js'); // import data.js
 sculptureListLengths = [];
 
 for(const sculpture in sculptureList){
-    // assigns the lengths of each key in a new, empty object
-    // pushes the new object to sculptureListLengths
-    sculptureListLengths.push(Object.assign({}, 
-        {
-            name: sculptureList[sculpture].name.length,
-            artist: sculptureList[sculpture].artist.length,
-            description: sculptureList[sculpture].description.length,
-            url: sculptureList[sculpture].url.length,
-            alt: sculptureList[sculpture].alt.length   
-        }
-    ));
+    const currentSculpture = sculptureList[sculpture];
+    for(const key in sculptureList[sculpture]){
+        currentSculpture[key] = currentSculpture[key].length;
+    }
+    sculptureListLengths.push(currentSculpture);
 }
 
 for (const sculpture in sculptureListLengths){
